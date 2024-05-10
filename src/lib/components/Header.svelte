816a1008a39abffa17d4900246a2b90512c2ef48
WesselvanDam <wesselvandam@hotmail.com>
2024-05-10 16:33:41 +0200

    feat(*): initial project structure

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,25 @@
+<script>
+
+</script>
+
+<div>
+  <img src="img/header.jpeg" alt="Header"/>
+  <div class="gradient-overlay">
+    <div class="text-8xl translate-x-1/2 translate-y-1/2">
+      <h1>Thank</h1>
+      <h1>You</h1>
+      <h1>Token</h1>
+    </div>
+  </div>
+</div>
+
+<style>
+  .gradient-overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(to right, transparent 0%, white 100%);
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<!-- import the header -->
+<script>
+  import Header from '$lib/components/Header.svelte';
+</script>
+
+<Header />
+<h1>Welcome to SvelteKit</h1>
+<p class="font-thin">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
