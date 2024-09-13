74f1b6baf6259ad3526a59c6f572351b8d398346
WesselvanDam <wesselvandam@hotmail.com>
2024-09-13 18:34:16 +0200

    Add privacy policy

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -2,33 +2,50 @@
   import Header from "$lib/components/Header.svelte";
 </script>
 
-<div class="flex flex-col items-center justify-center min-h-screen py-8">
+<div class="flex flex-col items-center justify-center min-h-screen pb-8">
   <Header />
   <div class="max-w-2xl px-4 py-8 text-left">
-    <h1 class="text-3xl font-bold mb-4">Privacy Policy</h1>
-    <p class="mb-4">
-      Your privacy is important to us. It is our policy to respect your privacy regarding any information we may collect from you across our website, and other sites we own and operate.
+    <h1 class="text-3xl font-bold">Privacy Policy</h1>
+    <h3 class="text-xl font-bold mt-6">Introduction</h3>
+    <p class="mt-2">
+      As the Thank You Token team, we respect your privacy and are committed to
+      protecting it. This Privacy Policy explains our policies and practices
+      regarding the collection, use, storage, and disclosure of your personal
+      information by the <i>Thank You Token App.</i>, available at
+      <a href="https://app.thank-you-token.nl" class="text-tyt_blue">
+        app.thank-you-token.nl</a
+      >.
     </p>
-    <p class="mb-4">
-      We only ask for personal information when we truly need it to provide a service to you. We collect it by fair and lawful means, with your knowledge and consent. We also let you know why we’re collecting it and how it will be used.
+    <h3 class="text-xl font-bold mt-6">Information We Collect</h3>
+    <p class="mt-2">
+      The Thank You Token web application uses the Google Drive API with the
+      <i>appDataScope</i> to store user data. This means we only have access to
+      the <i>appDataFolder</i> in your Google Drive, a special folder that is not
+      visible among your regular Google Drive folders and is accessible only by the
+      application that created it. Here, your submitted tokens and their metadata
+      (names, dates and descriptions) are stored. We do not collect or have access
+      to any other data in your Google Drive or Google Account.
     </p>
-    <p class="mb-4">
-      We only retain collected information for as long as necessary to provide you with your requested service. What data we store, we’ll protect within commercially acceptable means to prevent loss and theft, as well as unauthorized access, disclosure, copying, use or modification.
+    <h3 class="text-xl font-bold mt-6">How We Use Your Information</h3>
+    <p class="mt-2">
+      The data we store in your appDataFolder is used solely for the purpose of
+      enhancing your user experience in the Thank You Token app. We do not
+      share, sell, rent, or trade this data with third parties, and do not send
+      your data to any other form of database.
     </p>
-    <p class="mb-4">
-      We don’t share any personally identifying information publicly or with third-parties, except when required to by law.
+    <h3 class="text-xl font-bold mt-6">Changes to This Privacy Policy</h3>
+    <p class="mt-2">
+      We may update our Privacy Policy if deemed necessary. We will notify you
+      of any changes by posting the new Privacy Policy on this page.
     </p>
-    <p class="mb-4">
-      Our website may link to external sites that are not operated by us. Please be aware that we have no control over the content and practices of these sites, and cannot accept responsibility or liability for their respective privacy policies.
+    <h3 class="text-xl font-bold mt-6">Contact Us</h3>
+    <p class="mt-2">
+      If you have any questions about this Privacy Policy, please contact us
+      at
+      <a href="mailto:info@thank-you-token.nl" class="text-tyt_blue"
+        >info@thank-you-token.nl</a
+      >.
     </p>
-    <p class="mb-4">
-      You are free to refuse our request for your personal information, with the understanding that we may be unable to provide you with some of your desired services.
-    </p>
-    <p class="mb-4">
-      Your continued use of our website will be regarded as acceptance of our practices around privacy and personal information. If you have any questions about how we handle user data and personal information, feel free to contact us.
-    </p>
-    <p class="mb-4">
-      This policy is effective as of 1 January 2023.
-    </p>
+    <p class="mt-2">Last updated: September 13, 2024</p>
   </div>
 </div>
