92274cd96637943af787e6e8de6b6e33c45096b5
WesselvanDam <wesselvandam@hotmail.com>
2024-10-26 11:02:41 +0200

    fix(header): fix resizing behaviour on browser zoom

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,7 +1,7 @@
 <script>
 </script>
 
-<div class="relative">
+<div class="relative w-full">
   <img src="/img/header.jpg" alt="Header" class="w-full" />
   <div class="absolute top-1/2 w-2/5 -translate-y-1/2 right-0 -translate-x-1/4">
     <img src="/img/logo.svg" alt="Logo" class="w-full" />
